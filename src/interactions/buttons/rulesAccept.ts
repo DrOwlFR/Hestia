e1a337528cc09f11a6c6bcfc05cd7d3c2986fdd9
DrOwl <96383354+DrOwlFR@users.noreply.github.com>
2025-10-25 23:24:49 +0200

    feat(rulesAccept): Added code for the ‘rulesAccept’ button
    This button displays a modal window where you can enter the verification code.

---
+++ src/interactions/buttons/rulesAccept.ts
@@ -0,0 +1,34 @@
+import { Button } from "sheweny";
+import type { ShewenyClient } from "sheweny";
+import { ActionRowBuilder, ButtonInteraction, ModalBuilder, TextDisplayBuilder, TextInputBuilder, TextInputStyle } from "discord.js";
+
+export class RulesAcceptButton extends Button {
+	constructor(client: ShewenyClient) {
+		super(client, ["rulesAcceptButton"]);
+	}
+
+	async execute(button: ButtonInteraction) {
+
+		await button.showModal(
+			new ModalBuilder()
+				.setCustomId("verificationCodeModal")
+				.setTitle("Entrez votre code de vérification")
+				.addTextDisplayComponents(
+					new TextDisplayBuilder()
+						.setContent("**Rappel** : Pour obtenir votre code de vérification. Rendez-vous sur votre page de profil du site, et cliquez sur le bouton « Lier »."),
+				)
+				.addComponents(
+					new ActionRowBuilder<TextInputBuilder>()
+						.addComponents(
+							new TextInputBuilder({
+								custom_id: "verificationCode",
+								style: TextInputStyle.Short,
+								label: "Code de vérification",
+								required: true,
+							}),
+						),
+				),
+		);
+
+	}
+};
