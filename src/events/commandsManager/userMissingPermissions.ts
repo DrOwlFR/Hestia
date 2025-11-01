74d293afb57c2fb63e0d40af02c4a20707371628
DrOwl <96383354+DrOwlFR@users.noreply.github.com>
2025-11-01 18:45:46 +0100

    refactor(commands manager events): Modifying bot responses to be more roleplay-oriented.

--- src/events/commandsManager/userMissingPermissions.ts
+++ src/events/commandsManager/userMissingPermissions.ts
@@ -2,6 +2,7 @@ import type { ChatInputCommandInteraction, Permissions } from "discord.js";
 import { MessageFlags } from "discord.js";
 import type { ShewenyClient } from "sheweny";
 import { Event } from "sheweny";
+import stripIndent from "strip-indent";
 
 export class UserMissingPermissionsEvent extends Event {
 	constructor(client: ShewenyClient) {
@@ -15,7 +16,11 @@ export class UserMissingPermissionsEvent extends Event {
 	async execute(interaction: ChatInputCommandInteraction, missing: Permissions) {
 
 		return interaction.reply({
-			content: `<:round_cross:1424312051794186260> Vous n'avez pas la permission suffisante pour la commande \`${interaction}\`. Permission${missing.length > 1 ? "s" : ""} manquant${missing.length > 1 ? "es" : "e"} : *\`${missing}\`*.`,
+			content: stripIndent(`
+				> *Alors que vous essayez désespérément de faire fonctionner ce mécanisme, vous entendez des talons approcher en claquant sur le sol. Puis… La voix de la Concierge.*
+				Hep, hep, hep ! Que croyez-vous faire là ? Vous n'avez pas le droit ! Déguerpissez !\n
+				<:round_cross:1424312051794186260> Vous n'avez pas la permission suffisante pour la commande \`${interaction}\`. Permission${missing.length > 1 ? "s" : ""} manquant${missing.length > 1 ? "es" : "e"} : *\`${missing}\`*.
+				`),
 			flags: MessageFlags.Ephemeral,
 		});
 
