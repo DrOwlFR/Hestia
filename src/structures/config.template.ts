ea67ff9681ef86c9cec5ba5a5b6b4d6f38c1160d
DrOwl <96383354+DrOwlFR@users.noreply.github.com>
2025-11-07 19:27:44 +0100

    refactor(config.template.ts): Update the file to match the code updates.

--- src/structures/config.template.ts
+++ src/structures/config.template.ts
@@ -1,16 +1,44 @@
 export default {
 	DISCORD_TOKEN: "",
-	MONGO_TOKEN: "",
-	API_KEY: "",
 
-	conciergeLodgeChannelId: "",
+	MONGO_TOKEN: "",
+	dbName: "",
+
+	API_KEY: "",
+	APILink: "",
+
+	botAdminsIds: [""],
+	registeredGuildsIds: [""],
+
 	gardenGuildId: "",
+
+	seriousChannelId: "",
+	irlChannelId: "",
+	portraitGaleryChannelId: "",
+	antechamberChannelId: "",
+	loungeChannelId: "",
+
 	ampersandRoleId: "",
 	seedRoleId: "",
+
+	livingRoomRoleId: "",
+	workshopRoleId: "",
+	libraryRoleId: "",
+	terraceRoleId: "",
 	seriousRoleId: "",
-	seriousChannelId: "",
 	irlRoleId: "",
-	irlChannelId: "",
-	cadratinDiscordIds: [],
-	cadratinSiteIds: [],
+
+	adminRoleId: "",
+	modRoleId: "",
+	arobaseRoleId: "",
+	guillemetRoleId: "",
+
+	colorPrimary: 0xF0C841, // "#f0c841"
+	colorSecondary: 0xC98129, // "#c98129"
+	colorAccent: 0xA4345C, // "#a4345c"
+	colorTertiary: 0x639DA5, // "#639da5"
+
+	adminsDiscordIds: [""],
+	discordModsIds: [""],
+	siteModsIds: [""],
 };
