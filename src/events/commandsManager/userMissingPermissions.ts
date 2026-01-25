7ec7a1b4557419468e300ee36426ff5d5ad9d8a8
DrOwl <96383354+DrOwlFR@users.noreply.github.com>
2026-01-25 18:07:10 +0100

    fix(events/userMissingPermissions): reformat message content

--- src/events/commandsManager/userMissingPermissions.ts
+++ src/events/commandsManager/userMissingPermissions.ts
@@ -31,7 +31,7 @@ export class UserMissingPermissionsEvent extends Event {
 			content: stripIndent(`
 				> *Alors que vous essayez désespérément de faire fonctionner ce mécanisme, vous entendez des talons approcher en claquant sur le sol. Puis… La voix de la Concierge.*
 				Hep, hep, hep ! Que croyez-vous faire là ? Vous n'avez pas le droit ! Déguerpissez !\n
-				${config.emojis.cross} Vous n'avez pas la permission suffisante pour la commande \`${interaction}\`. Permission${missing.length > 1 ? "s" : ""} manquant${missing.length > 1 ? "es" : "e"} : *\`${missing}\`*.
+				-# ${config.emojis.cross} Vous n'avez pas la permission suffisante pour la commande \`${interaction}\`. Permission${missing.length > 1 ? "s" : ""} manquant${missing.length > 1 ? "es" : "e"} : *\`${missing}\`*.
 				`),
 			flags: MessageFlags.Ephemeral,
 		});
