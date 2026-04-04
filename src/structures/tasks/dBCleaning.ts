2048e6ac73d54ea40a032e6bfed31c6cf5f23b09
DrOwl <96383354+DrOwlFR@users.noreply.github.com>
2026-04-04 19:09:56 +0200

    refactor(tasks/dbCleaning): add of support for a new scenario
    Handling cases where an unconfirmed user (excluding promotion and demotion) has accessory roles

--- src/structures/tasks/dBCleaning.ts
+++ src/structures/tasks/dBCleaning.ts
@@ -3,7 +3,7 @@ import Bottleneck from "bottleneck";
 import type { Guild, TextChannel } from "discord.js";
 import type { ShewenyClient } from "sheweny";
 
-import type { responseJson } from "../../types";
+// import type { responseJson } from "../../types";
 import config from "../config";
 import { LinkedUser, User } from "../database/models";
 
@@ -151,104 +151,114 @@ export async function dailyDBCleaning(gardenGuild: Guild, client: ShewenyClient,
 		const linkedUsers = await LinkedUser.find();
 		const allLogs: string[] = [];
 
-		const linkedUserTasks = linkedUsers.map(dbLinkedUser =>
-			async () => {
-				try {
-					const getResponse = await getUserLimiter.schedule(() =>
-						client.functions.getUser(dbLinkedUser.discordId),
+		const linkedUserTasks = linkedUsers.map(async dbLinkedUser => {
+			try {
+				const getResponse = await getUserLimiter.schedule(() =>
+					client.functions.getUser(dbLinkedUser.discordId),
+				);
+				// If the user is not found on the site, remove roles and delete from DB
+				if (getResponse.status === 404) {
+					await fetchLimiter.schedule(async () => {
+						const member = await gardenGuild.members.fetch(dbLinkedUser.discordId).catch(() => null);
+						if (member) {
+							const rolesToRemove = [
+								config.confirmedUserRoleId,
+								config.nonConfirmedUserRoleId,
+								config.livingRoomRoleId,
+								config.workshopRoleId,
+								config.libraryRoleId,
+								config.terraceRoleId,
+								config.seriousRoleId,
+								config.irlRoleId,
+							];
+							const memberRoles = member.roles;
+							const rolesOwned = rolesToRemove.filter(roleId => memberRoles.cache.has(roleId));
+
+							if (rolesOwned.length > 0) {
+								await memberRoles.remove(rolesOwned);
+							}
+						}
+					});
+
+					await deleteLimiter.schedule(() =>
+						client.functions.deleteUser(dbLinkedUser.discordId),
 					);
-					// If the user is not found on the site, remove roles and delete from DB
-					if (getResponse.status === 404) {
-						await fetchLimiter.schedule(async () => {
-							const member = await gardenGuild.members.fetch(dbLinkedUser.discordId).catch(() => null);
-							if (member) {
-								const rolesToRemove = [
-									config.confirmedUserRoleId,
-									config.nonConfirmedUserRoleId,
-									config.livingRoomRoleId,
-									config.workshopRoleId,
-									config.libraryRoleId,
-									config.terraceRoleId,
-									config.seriousRoleId,
-									config.irlRoleId,
-								];
-								const memberRoles = member.roles;
-								const rolesOwned = rolesToRemove.filter(roleId => memberRoles.cache.has(roleId));
-
-								if (rolesOwned.length > 0) {
-									await memberRoles.remove(rolesOwned);
-								}
+					await LinkedUser.deleteOne({ discordId: dbLinkedUser.discordId });
+					allLogs.push(`-# 🧹 La connexion au site de l'ID \`${dbLinkedUser.discordId}\` (siteId : \`${dbLinkedUser.siteId}\`, username : \`${dbLinkedUser.discordUsername}\`) a été supprimée car il n'est plus sur le Manoir.`);
+				} else {
+					await fetchLimiter.schedule(async () => {
+						const member = await gardenGuild.members.fetch(dbLinkedUser.discordId).catch(() => null);
+						// If the user exists on the site, sync roles
+						if (member) {
+							// Sync roles between DB and site account
+							// const getResponseJson = await getResponse.json() as responseJson;
+							// const rolesApi = getResponseJson.roles || [];
+							const rolesApi = ["user"];
+							const rolesDb = dbLinkedUser.roles;
+							const rolesDiffer = rolesApi.length !== rolesDb.length || !rolesApi.every(role => rolesDb.includes(role));
+							if (rolesDiffer) {
+								await LinkedUser.findOneAndUpdate(
+									{ discordId: dbLinkedUser.discordId },
+									{ roles: rolesApi },
+								);
 							}
-						});
 
-						await deleteLimiter.schedule(() =>
-							client.functions.deleteUser(dbLinkedUser.discordId),
-						);
-						await LinkedUser.deleteOne({ discordId: dbLinkedUser.discordId });
-						allLogs.push(`-# 🧹 La connexion au site de l'ID \`${dbLinkedUser.discordId}\` (siteId : \`${dbLinkedUser.siteId}\`, username : \`${dbLinkedUser.discordUsername}\`) a été supprimée car il n'est plus sur le Manoir.`);
-					} else {
-						await fetchLimiter.schedule(async () => {
-							const member = await gardenGuild.members.fetch(dbLinkedUser.discordId).catch(() => null);
-							// If the user exists on the site, sync roles
-							if (member) {
-								// Sync roles between DB and site account
-								const getResponseJson = await getResponse.json() as responseJson;
-								const rolesApi = getResponseJson.roles || [];
-								const rolesDb = dbLinkedUser.roles;
-								const rolesDiffer = rolesApi.length !== rolesDb.length || !rolesApi.every(role => rolesDb.includes(role));
-								if (rolesDiffer) {
-									await LinkedUser.findOneAndUpdate(
-										{ discordId: dbLinkedUser.discordId },
-										{ roles: rolesApi },
-									);
-								}
+							// Determine confirmation status from site roles
+							const isNonConfirmed = rolesApi.includes("user");
+							const isConfirmed = rolesApi.includes("user-confirmed");
 
-								// Determine confirmation status from site roles
-								const isNonConfirmed = rolesApi.includes("user");
-								const isConfirmed = rolesApi.includes("user-confirmed");
+							// Accessory roles
+							const accessoryRoles = [config.livingRoomRoleId,
+								config.workshopRoleId,
+								config.libraryRoleId,
+								config.terraceRoleId,
+								config.seriousRoleId,
+								config.irlRoleId];
 
-								// Determine current roles in guild
-								const memberRoles = member.roles;
-								const hasNonConfirmedRole = memberRoles.cache.has(config.nonConfirmedUserRoleId);
-								const hasConfirmedRole = memberRoles.cache.has(config.confirmedUserRoleId);
+							// Determine current roles in guild
+							const memberRoles = member.roles;
+							const hasNonConfirmedRole = memberRoles.cache.has(config.nonConfirmedUserRoleId);
+							const hasConfirmedRole = memberRoles.cache.has(config.confirmedUserRoleId);
+							const hasAccessoryRoles = accessoryRoles.some(roleId => memberRoles.cache.has(roleId));
 
-								// Determine roles to add or remove
-								const toAdd: string[] = [];
-								const toRemove: string[] = [];
+							// Determine roles to add or remove
+							const toAdd: string[] = [];
+							const toRemove: string[] = [];
 
-								if (isNonConfirmed && !hasNonConfirmedRole) {
-									toAdd.push(config.nonConfirmedUserRoleId);
-									allLogs.push(`-# ➕ Rôle Graine de l'ID \`${member.user.id}\` ajouté.`);
-								} else if (!isNonConfirmed && hasNonConfirmedRole) {
-									toRemove.push(config.nonConfirmedUserRoleId);
-									allLogs.push(`-# 🧹 Rôle Graine de l'ID \`${member.user.id}\` retiré.`);
-								}
-
-								if (isConfirmed && !hasConfirmedRole) {
-									toAdd.push(config.confirmedUserRoleId);
-									allLogs.push(`-# ➕ Rôle Esperluette de l'ID \`${member.user.id}\` ajouté.`);
-								} else if (!isConfirmed && hasConfirmedRole) {
-									toRemove.push(config.confirmedUserRoleId,
-										config.livingRoomRoleId,
-										config.workshopRoleId,
-										config.libraryRoleId,
-										config.terraceRoleId,
-										config.seriousRoleId,
-										config.irlRoleId);
-									allLogs.push(`-# 🧹 Rôles Esperluette et accessoires de l'ID \`${member.user.id}\` retiré.`);
-								}
-
-								// Apply role changes
-								if (toAdd.length) await memberRoles.add(toAdd);
-								if (toRemove.length) await memberRoles.remove(toRemove);
+							if (isNonConfirmed && !hasNonConfirmedRole) {
+								toAdd.push(config.nonConfirmedUserRoleId);
+								allLogs.push(`-# ➕ Rôle Graine de l'ID \`${member.user.id}\` ajouté.`);
+							} else if (!isNonConfirmed && hasNonConfirmedRole) {
+								toRemove.push(config.nonConfirmedUserRoleId);
+								allLogs.push(`-# 🧹 Rôle Graine de l'ID \`${member.user.id}\` retiré.`);
 							}
-						});
-					}
-				} catch (err) {
-					console.error(`Erreur lors de la suppression du LinkedUser ${dbLinkedUser.discordId}:`, err);
-					logChannel.send(`Erreur lors de la vérification du LinkedUser \`${dbLinkedUser.discordId}\` : ${err}`);
+
+							if (isNonConfirmed && hasAccessoryRoles) {
+								toRemove.push(...accessoryRoles);
+								allLogs.push(`-# 🧹 Rôles accessoires de l'ID \`${member.user.id}\` retiré car ce n'est pas une Esperluette.`);
+							}
+
+							if (isConfirmed && !hasConfirmedRole) {
+								toAdd.push(config.confirmedUserRoleId);
+								allLogs.push(`-# ➕ Rôle Esperluette de l'ID \`${member.user.id}\` ajouté.`);
+							} else if (!isConfirmed && hasConfirmedRole) {
+								toRemove.push(config.confirmedUserRoleId,
+									...accessoryRoles,
+								);
+								allLogs.push(`-# 🧹 Rôles Esperluette et accessoires de l'ID \`${member.user.id}\` retiré.`);
+							}
+
+							// Apply role changes
+							if (toAdd.length) await memberRoles.add(toAdd);
+							if (toRemove.length) await memberRoles.remove(toRemove);
+						}
+					});
 				}
-			});
+			} catch (err) {
+				console.error(`Erreur lors de la suppression du LinkedUser ${dbLinkedUser.discordId}:`, err);
+				logChannel.send(`Erreur lors de la vérification du LinkedUser \`${dbLinkedUser.discordId}\` : ${err}`);
+			}
+		});
 
 		await Promise.all(linkedUserTasks);
 
